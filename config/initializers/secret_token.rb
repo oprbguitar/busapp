07904e20e77a85398bcccf297cd7754b9b6a94be
Pierre <oprbguitar@gmail.com>
2013-08-01 22:14:29 -0500

    testing file
    file for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busapp::Application.config.secret_token = 'cc2d7fe13b5e3922eb1935c6b804da2c46733d5f2a5109cf077ca50107ccaafa82dee22dd7e2d1bd8a4e00a39d9354d0ce2986cedbb33ba3ab62cad5cae462ca'
